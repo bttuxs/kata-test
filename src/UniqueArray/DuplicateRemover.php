46810fd968ad585280f2dc90f42af49abba2c2d6
baldomero de castilla <bttuxs@gmail.com>
2022-10-04 13:48:11 -0500

    Is solved by removing duplicates on an array

--- src/UniqueArray/DuplicateRemover.php
+++ src/UniqueArray/DuplicateRemover.php
@@ -9,6 +9,22 @@ class DuplicateRemover
         /**
          * @todo
          */
-        return [];
+         $lengthBase = count($input);
+         $dataClean = [];
+
+
+         for($idxBase=0; $idxBase < $lengthBase; $idxBase++){
+           $addItem = true;
+           for($idxClean=0; $idxClean < count($dataClean); $idxClean++){
+             if($input[$idxBase] == $dataClean[$idxClean]){
+               $addItem = false;
+               break;
+             }
+           }
+           if($addItem){
+             $dataClean[] = $input[$idxBase];
+           }
+         }
+         return $dataClean;
     }
 }
