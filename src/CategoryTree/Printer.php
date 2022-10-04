6daea4d9f3738bafe4ac374b9b71bab9dba107c0
baldomero de castilla <bttuxs@gmail.com>
2022-10-04 13:39:49 -0500

    Is solved by the category tree

--- src/CategoryTree/Printer.php
+++ src/CategoryTree/Printer.php
@@ -4,11 +4,25 @@ namespace Kata\CategoryTree;
 
 class Printer
 {
-    public function build(Category $parent, int $spaces = 0): string
+  public function getNameChild(Category $item, int $spaces):string
+  {
+    $strNames = str_repeat("  ", $spaces).$item->name()."\n";
+    if(count($item->children())> 0)
     {
-        /**
-         * @todo
-         */
-        return '';
+      $data =$item->children();
+      usort($data, fn($itemA, $itemB) => $itemA->name() <=> $itemB->name());
+      foreach($data as $child){
+        $strNames.= $this->getNameChild($child, $spaces+1);
+      }
     }
+    return $strNames;
+  }
+
+  public function build(Category $parent, int $spaces = 0): string
+  {
+      /**
+       * @todo
+       */
+      return $this->getNameChild($parent, $spaces);
+  }
 }
